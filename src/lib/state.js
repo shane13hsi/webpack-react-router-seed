18472a4d00c1c3a912c45bdd55785b8e9dca2f26
shanshan xi <shanshan.xi@dianping.com>
2015-04-26 21:03:56 +0800

    Remove flowtype for now

--- src/lib/state.js
+++ src/lib/state.js
@@ -1,47 +1,57 @@
 import EventEmitter from 'eventemitter3';
 import Immutable from 'immutable';
 
-export default class State extends EventEmitter {
+export default
+class State extends EventEmitter {
 
-  constructor(state, reviver: ?Function) {
-    super();
-    this._state = null;
-    this._reviver = reviver;
-    this.load(state || {});
-  }
+    /**
+     * @param reviver optional, Function
+     * */
+    constructor(state, reviver) {
+        super();
+        this._state = null;
+        this._reviver = reviver;
+        this.load(state || {});
+    }
 
-  load(state: Object) {
-    this.set(Immutable.Map.isMap(state)
-      ? state
-      : Immutable.fromJS(state, this._reviver)
-    );
-  }
+    /**
+     * @param state Object
+     * */
+    load(state) {
+        this.set(Immutable.Map.isMap(state)
+                ? state
+                : Immutable.fromJS(state, this._reviver)
+        );
+    }
 
-  set(state) {
-    if (this._state === state) return;
-    this._state = state;
-    this.emit('change', this._state);
-  }
+    set(state) {
+        if (this._state === state) return;
+        this._state = state;
+        this.emit('change', this._state);
+    }
 
-  get() {
-    return this._state;
-  }
+    get() {
+        return this._state;
+    }
 
-  save(): Object {
-    return this._state.toJS();
-  }
+    /**
+     * @return Object
+     * */
+    save() {
+        return this._state.toJS();
+    }
 
-  toConsole() {
-    console.log(JSON.stringify(this.save())); // eslint-disable-line no-console
-  }
+    toConsole() {
+        console.log(JSON.stringify(this.save())); // eslint-disable-line no-console
+    }
 
-  cursor(path) {
-    return (update) => {
-      if (update)
-        this.set(this._state.updateIn(path, update));
-      else
-        return this._state.getIn(path);
-    };
-  }
+    cursor(path) {
+        return (update) => {
+            if (update)
+                this.set(this._state.updateIn(path, update));
+            else
+                return this._state.getIn(path);
+        };
+    }
 
 }
