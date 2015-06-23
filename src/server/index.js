a3077a30aaf36d671bc2ac4c9b86f1e81819f3c5
Shane Hsi <shane13hsi@gmail.com>
2015-06-23 18:14:40 +0800

    FixMe

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,6 @@
 const config = require('./config');
+import express from './express';
+import path from 'path';
 
 // piping Keep your code piping hot! Live code reloading without additional binaries
 if (require('piping')(config.piping)) {
@@ -11,5 +13,7 @@ if (require('piping')(config.piping)) {
         };
     });
 
-    require('./main');
+    express({
+        indexHtmlPath: path.join(__dirname, '/index.html')
+    });
 }
