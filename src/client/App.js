4236525824ce78c11c69c2b378c5073b70cb981e
Shane Hsi <shane13hsi@gmail.com>
2015-06-26 07:35:08 +0800

    Using pure function

--- src/client/App.js
+++ src/client/App.js
@@ -2,22 +2,27 @@ import React, { PropTypes } from 'react';
 import DocumentTitle from 'react-document-title';
 import { RouteHandler } from 'react-router';
 
-const App = React.createClass({
-  propTypes: {
+const App = (props) => {
+
+  App.propTypes = {
     params: PropTypes.object.isRequired,
     query: PropTypes.object.isRequired
-  },
+  };
 
-  render() {
-    return (
-      <DocumentTitle title='Sample App'>
-        <div className='App'>
-          <h2>Sample App</h2>
-          <RouteHandler {...this.props} />
-        </div>
-      </DocumentTitle>
-    );
-  }
-});
+  return {
+    props,
+
+    render() {
+      return (
+        <DocumentTitle title='Sample App'>
+          <div className='App'>
+            <h2>Sample App</h2>
+            <RouteHandler {...this.props} />
+          </div>
+        </DocumentTitle>
+      );
+    }
+  };
+};
 
 export default App;
